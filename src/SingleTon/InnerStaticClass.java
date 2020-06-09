307a0165622a0d5f50ddf4f6175332105c0e8aa4
Song-Fang <sof10@pitt.edu>
2020-06-09 19:44:02 -0400

    singleton different ways to write

--- src/SingleTon/InnerStaticClass.java
+++ src/SingleTon/InnerStaticClass.java
@@ -1,4 +1,15 @@
 package SingleTon;
 
 public class InnerStaticClass {
+    private static class InnerClassHolder{
+        public static InnerStaticClass innerStaticClass = new InnerStaticClass();
+    }
+
+    private InnerStaticClass(){
+
+    }
+
+    public static InnerStaticClass getInstance(){
+        return InnerClassHolder.innerStaticClass;
+    }
 }
