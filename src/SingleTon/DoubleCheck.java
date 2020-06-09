5071d81538a41e612b2d18942d93fd1904fb83de
Song-Fang <sof10@pitt.edu>
2020-06-09 19:35:55 -0400

    lock control

--- src/SingleTon/DoubleCheck.java
+++ src/SingleTon/DoubleCheck.java
@@ -1,4 +1,21 @@
 package SingleTon;
 
 public class DoubleCheck {
+    private static DoubleCheck doubleCheck;
+
+    private DoubleCheck(){
+
+    }
+
+    public static DoubleCheck getInstance(){
+        if(doubleCheck==null){
+            synchronized (DoubleCheck.class){
+                if(doubleCheck==null){
+                    doubleCheck = new DoubleCheck();
+                    return doubleCheck;
+                }
+            }
+        }
+        return doubleCheck;
+    }
 }
